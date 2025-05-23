d7112247a4643a435988c621e01a28c1aebdcd62
dhhaarmik <dharmik2114@gmail.com>
2025-05-23 17:54:33 +0530

    Update htmlTemplates.py

--- htmlTemplates.py
+++ htmlTemplates.py
@@ -1,45 +1,25 @@
-
-css = '''
+css = """
 <style>
-.chat-message {
-    padding: 1.5rem; border-radius: 0.5rem; margin-bottom: 1rem; display: flex
-}
-.chat-message.user {
-    background-color: #2b313e
-}
-.chat-message.bot {
-    background-color: #475063
-}
-.chat-message .avatar {
-  width: 20%;
-}
-.chat-message .avatar img {
-  max-width: 78px;
-  max-height: 78px;
-  border-radius: 50%;
-  object-fit: cover;
-}
-.chat-message .message {
-  width: 80%;
-  padding: 0 1.5rem;
-  color: #fff;
-}
-'''
+    .justified {
+        text-align: justify;
+        font-size: 16px;
+        line-height: 1.5;
+    }
+    .chat-bubble {
+        border-radius: 12px;
+        padding: 12px;
+        margin: 10px 0;
+        font-size: 16px;
+        line-height: 1.6;
+        text-align: justify;
+    }
+</style>
+"""
 
-bot_template = '''
-<div class="chat-message bot">
-    <div class="avatar">
-        <img src="https://i.ibb.co/cN0nmSj/Screenshot-2023-05-28-at-02-37-21.png" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
-    </div>
-    <div class="message">{{MSG}}</div>
-</div>
-'''
+user_template = """
+<div class="chat-bubble" style="background-color: #DCF8C6;"><strong>User:</strong><br>{{MSG}}</div>
+"""
 
-user_template = '''
-<div class="chat-message user">
-    <div class="avatar">
-        <img src="https://i.ibb.co/rdZC7LZ/Photo-logo-1.png">
-    </div>    
-    <div class="message">{{MSG}}</div>
-</div>
-'''
+bot_template = """
+<div class="chat-bubble" style="background-color: #F1F0F0;"><strong>Bot:</strong><br>{{MSG}}</div>
+"""
